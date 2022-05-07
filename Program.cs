254072a7c208a8070a08d6f538a477aef93fae50
cjohnson <collin.g.johnson@gmail.com>
2022-05-07 15:14:54 -0400

    Number conversions

--- Program.cs
+++ Program.cs
@@ -1,7 +1,17 @@
 ﻿class TestClass
 {
-    static int Main()
+    static void Main(string[] args)
     {
-        return 0;
+        // Convert first string into long type
+        long num = Int64.Parse(args[0]);
+        System.Console.WriteLine(num);
+
+        // Use long type to parse, aliasing Int64.
+        num = long.Parse(args[0]);
+        System.Console.WriteLine(num);
+
+        // Use Convert class method ToInt64 to do the same as the previous two.
+        num = Convert.ToInt64(args[0]);
+        System.Console.WriteLine(num);
     }
 }
